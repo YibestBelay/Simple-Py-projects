72d7e39bff67f34bf911e0b953d938039ba061ac
YibestBelay <yibestbelay@gmail.com>
2025-06-30 22:21:26 +0300

    madLib

---
+++ madlib.py
@@ -0,0 +1,5 @@
+adj = input("adjective: ")
+ver = input("verb: ")
+famous = input("SuperHero: ")
+madlib = f"\nlazyComputer programming is so {adj}! I am having a great time by just {ver} codes.It feel like you are like {famous} having super natural power.\n"
+print(madlib)
